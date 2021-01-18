082625143fa12d8a6af7a49d73f9c20141a98f15
annoniyous <bouysantiman.y@outlook.com>
2021-01-18 23:34:42 +0100

    Exo fini

--- src/App.js
+++ src/App.js
@@ -8,13 +8,29 @@ export class App extends Component {
 
     title : "Titre par default",
 
-    para : "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
+    para : "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+    titre2 : "Titre2",
+    npara : "Lorem ipsum, dolor sit amet consectetur adipisicing elit"
   }
   
   changeTitle = ()=> {
       this.setState({title:"Mon title"} );
    }
+   valInput = () =>{
+     let input = document.querySelector('input');
+     let val = input.value;
+     this.setState({
+       titre2 : val
+     })
+   }
+   changeText = () =>{
+     let aria = document.querySelector("textarea");
+     let valarea = aria.value;
+     this.setState({
+       npara : valarea
+     })
 
+   }
    
    render() {
      
@@ -25,6 +41,11 @@ export class App extends Component {
         <button onClick={() =>{
         this.setState({para: "Mon nouveau para"})
       }}>Changer le para</button>
+      <h2>{this.state.titre2}</h2>
+      <input type="text" name="" id=""/>
+      <button onClick={this.valInput}>Changer le titre 2 en input</button>
+      <p>{this.state.npara}</p>
+      <textarea onChange={this.changeText} name="" id="" cols="30" rows="10"></textarea>
       </div>
       
     )
